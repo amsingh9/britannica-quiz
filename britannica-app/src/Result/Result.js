f1b373e4140a5ec6ce65efc4b26558507d9b0883
Amanjot Singh <amanpabla@gmail.com>
2018-03-25 23:39:00 -0400

    adding css to result page

--- britannica-app/src/Result/Result.js
+++ britannica-app/src/Result/Result.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import './Result.css'
 
 class ResultPage extends Component {
     render () {
         const result = this.props.updatedscore >2 ? <p> Good Job !! You got {this.props.updatedscore} answers correct ! </p> : <p> You got {this.props.updatedscore} answers correct. Better Luck next time. </p>;
         return (
-            <div>
+            <div className="result">
                 {result}
             </div>
         );
